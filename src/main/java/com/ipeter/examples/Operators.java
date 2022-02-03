f65de61764108ee80531c329e5da1655cc7f97d4
Peter Izak <peter.izak@msg-life.com>
2022-02-03 08:24:33 +0000

    second attempt

---
+++ src/main/java/com/ipeter/examples/Operators.java
@@ -0,0 +1,42 @@
+package com.ipeter.examples;
+
+
+public class Operators {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("-------------------");
+        System.out.println("test1");
+        test1();
+
+        System.out.println("-------------------");
+        System.out.println("test2");
+        test2();
+
+    }
+
+    private static void test1() {
+        int v = 127;
+        if (v++ > 128 && ++v == 128 & ++v == 129 && ++v == 129) {
+            System.out.println("127");
+        }
+        System.out.println(v);
+        if (v++ < 128 || ++v == 128 | v-- == 129) {
+            System.out.println("128");
+        }
+        System.out.println("v = " + v);
+    }
+
+    public static void test2() {
+        int v = 127;
+        if (v++ < 128 && v++ == 128 | ++v == 129 | ++v == 129 & ++v == 129) {
+            System.out.println("127");
+        }
+        System.out.println(v);
+        if (v++ < 128 || ++v == 128 | v-- == 129) {
+            System.out.println("128");
+        }
+        System.out.println("v = " + v);
+    }
+}
