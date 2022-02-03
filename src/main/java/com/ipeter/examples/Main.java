b5b708755a6a653d928069d04529d1e101545f19
Peter Izak <peter.izak@msg-life.com>
2022-02-03 08:20:23 +0000

    first attempt

---
+++ src/main/java/com/ipeter/examples/Main.java
@@ -0,0 +1,9 @@
+package com.ipeter.examples;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Double ret = 1 / 0.0 + 3.0;
+        System.out.println("Hello World:" + ret);
+    }
+}
